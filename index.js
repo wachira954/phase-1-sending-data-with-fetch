752c76f1fe5899f19afffbd501ccb8de239bc101
wachira <wachiraa954@gmail.com>
2022-06-20 12:13:37 +0300

    commit tree afec982f260d659b6cae2429a15c1d042b634085

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+function submitData(name, email){
+    const configurationObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        })
+    }
+
+    return fetch ('http://localhost:3000/users', configurationObject)
+    .then((resp)=> resp.json())
+    .then((json)=> document.body.innerHTML = json.id)
+    .catch((error)=> {
+        document.body.innerHTML = error.message
+    })
+}
